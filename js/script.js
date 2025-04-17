3767ad40ca0689239d13bfc19e6762b5de7f68ba
Raymond Cato <rlcato1@student.fullsail.edu>
2025-04-17 11:51:11 -0400

    Updated styling and added functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-// Show the button when scrolled down 200px
+// Get the button
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    document.getElementById("back-to-top").style.display = "block";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    document.getElementById("back-to-top").style.display = "none";
+    mybutton.style.display = "none";
   }
 };
 
-// Scroll to top when clicked
-document.getElementById("back-to-top").onclick = function() {
+// Scroll to the top of the document when the button is clicked
+function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
-};
+}
